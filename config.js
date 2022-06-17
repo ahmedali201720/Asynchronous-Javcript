76f1d8912a2bd68da47c2a8a6e5e8aa9fd017e64
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2022-06-17 14:59:09 +0200

    test promises behaviour with fetch function

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const apiKey = "12447ee0130326f364be5a29b1a7e503";
-export const apiURL = `http://api.weatherstack.com/? access_key = ${apiKey}`;
+export const apiURL = `http://api.weatherstack.com/current? access_key = ${apiKey}& query = New York`;
 
 // http://api.weatherstack.com/current
 //     ? access_key = YOUR_ACCESS_KEY
--- promises.js
+++ promises.js
@@ -1,1 +1,8 @@
 import { apiURL } from "./config.js";
+
+const options = {
+  mode: "no-cors",
+};
+
+fetch(apiURL, options).then((response) => console.log(response));
+console.log("Welcome Ahmed ALi");
