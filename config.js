334bc1706e4ba5b1cc98de92d7526029b2055282
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2022-06-17 14:34:18 +0200

    setting up remote API config

---
+++ config.js
@@ -0,0 +1,6 @@
+const apiKey = "12447ee0130326f364be5a29b1a7e503";
+export const apiURL = `http://api.weatherstack.com/? access_key = ${apiKey}`;
+
+// http://api.weatherstack.com/current
+//     ? access_key = YOUR_ACCESS_KEY
+//     & query = New York
---
+++ promises.js
@@ -0,0 +1,1 @@
+import { apiURL } from "./config.js";
